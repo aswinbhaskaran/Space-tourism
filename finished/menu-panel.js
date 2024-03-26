bf32587b7f8fb40c823701dc94967709d95eca8c
aswinbhaskaran <66071969+aswinbhaskaran@users.noreply.github.com>
2024-03-26 15:20:55 +0530

    Updated menu-panel.js

--- finished/menu-panel.js
+++ finished/menu-panel.js
@@ -1,26 +1,28 @@
 window.onresize = function()
 {
+	const panel = document.querySelector('#header #menu-panel')
+	const img = panel.querySelector('#img')
+	const close = panel.querySelector('#menu-panel-close')
+
 	if((screen.width >= 375) && (screen.width < 768))
 	{
 		function hide()
 		{
-			const menu = document.getElementById('menu-panel-visible');
-
-			if(!menu.classList.contains('d-none'))
-				menu.classList.add('d-none');
+			panel.classList.replace('d-flex', 'd-none')
 		}
 
 		function show()
 		{
-			const menu = document.getElementById('menu-panel-visible');
-
-			if(menu.classList.contains('d-none'))
-				menu.classList.remove('d-none');
+			panel.classList.replace('d-none', 'd-flex')
 		}
 
+		panel.classList.add('d-none')
 		document.getElementById('mob-menu').onclick = show;
 		document.getElementById('mob-menu-close').addEventListener('click', hide);
 		document.querySelector('main').onclick = hide;
 	}
+	else
+		if(panel.getAttribute('class') != null)
+			panel.removeAttribute('class')
 }
 
