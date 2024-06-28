915c4d9658db781c7a3ff00ae6cd6c9567dcce08
aswinbhaskaran <66071969+aswinbhaskaran@users.noreply.github.com>
2024-06-28 10:06:04 +0000

    Files updated

--- menu-panel.js
+++ menu-panel.js
@@ -1,28 +1,40 @@
+const [header, main] = [document.getElementById('header'), document.querySelector('main')]
+const [panel, mobMenu, mobMenuClose] = [header.querySelector('#menu-panel'), header.querySelector('#mob-menu'), header.querySelector('#mob-menu-close')]
+
+function hide()
+{
+	panel.classList.replace('d-flex', 'd-none')
+}
+
+function show()
+{
+	panel.classList.replace('d-none', 'd-flex')
+}
+
 window.onresize = function()
 {
-	const panel = document.querySelector('#header #menu-panel')
-	const img = panel.querySelector('#img')
-	const close = panel.querySelector('#menu-panel-close')
-
 	if((screen.width >= 375) && (screen.width < 768))
 	{
-		function hide()
-		{
-			panel.classList.replace('d-flex', 'd-none')
-		}
-
-		function show()
-		{
-			panel.classList.replace('d-none', 'd-flex')
-		}
-
 		panel.classList.add('d-none')
-		document.getElementById('mob-menu').onclick = show;
-		document.getElementById('mob-menu-close').addEventListener('click', hide);
-		document.querySelector('main').onclick = hide;
+		mobMenu.onclick = show;
+		mobMenuClose.addEventListener('click', hide)
+		main.onclick = hide
 	}
 	else
 		if(panel.getAttribute('class') != null)
 			panel.removeAttribute('class')
 }
 
+document.querySelector('body').onload = function()
+{
+	if((screen.width >= 375) && (screen.width < 768))
+	{
+		panel.classList.add('d-none')
+		mobMenu.onclick = show
+		mobMenuClose.addEventListener('click', hide)
+		main.onclick = hide
+	}
+	else
+		if(panel.getAttribute('class') != null)
+			panel.removeAttribute('class')
+}
